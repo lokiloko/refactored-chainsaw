1d1a03e0c7b3655e00f0b3315f482ddfe1bce614
Ian Winanto <ian.winanto@tanihub.com>
2021-09-04 15:27:19 +0700

    [ADD] Implementation of solveAnagram

--- logic-test/main.go
+++ logic-test/main.go
@@ -1,12 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 func main() {
 	// Driver Code
 	testCase1 := []string{"kita", "atik", "tika", "aku", "kia", "makan", "kua"}
 
-	solveAnagram(testCase1)
+	fmt.Println(solveAnagram(testCase1))
 }
 
-func solveAnagram(testCase []string) {
-	
+func solveAnagram(testCase []string) [][]string {
+	result := [][]string{}
+	anagrams := map[string][]string{}
+
+	for _, str := range testCase {
+		sortedStr := sortString(str)
+		anagrams[sortedStr] = append(anagrams[sortedStr], str)
+	}
+
+	for _, anagram := range anagrams {
+		result = append(result, anagram)
+	}
+
+	return result
+}
+
+func sortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
 }
