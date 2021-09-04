3220c71bc1c873c65a8d61abf06e28c4d5cce25c
Ian Winanto <ian.winanto@tanihub.com>
2021-09-04 15:10:12 +0700

    [ADD] Initialize project and driver code

---
+++ logic-test/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// Driver Code
+	testCase1 := []string{"kita", "atik", "tika", "aku", "kia", "makan", "kua"}
+
+	solveAnagram(testCase1)
+}
+
+func solveAnagram(testCase []string) {
+	
+}
